428c7257b833d2ca145f99aa690d543809618a0e
toamitkumar@gmail.com <>
2012-06-06 04:58:34 +0000

    Cleaned landing page and added canary test

---
+++ src/test/java/CanaryTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CanaryTest {
+
+  @Test
+  public void testShouldPass() {
+    assertEquals(true, true);
+  }
+
+}
